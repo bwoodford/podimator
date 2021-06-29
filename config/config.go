e01f2b2670fd7b71206b7b0c56a7fe7c32cb1445
Brandon Woodford <ivegot@norto.dev>
2021-06-29 18:20:46 -0500

    Add config interfaces for unit testing
    Adding interface implementations to override the default behavior of the
    OS package. Unit tests can now be written to check for basic error
    handling.

--- config/config.go
+++ config/config.go
@@ -2,11 +2,28 @@ package config
 
 import (
 	"fmt"
+	"io"
+	"io/fs"
 	"io/ioutil"
 	"os"
 
-	"github.com/pelletier/go-toml"
+	"github.com/pelletier/go-toml/v2"
 )
+
+var fsym fileSystem = osFS{}
+
+type osFS struct{}
+
+type fileSystem interface {
+	Open(path string) (file, error)
+	MkdirAll(path string, perm fs.FileMode) error
+	ReadAll(r io.Reader) ([]byte, error)
+}
+
+type file interface {
+	io.Closer
+	io.Reader
+}
 
 type Podcast struct {
 	URL  string
@@ -18,67 +35,43 @@ type Config struct {
 	Podcasts []*Podcast
 }
 
-// Add error return
-func (config *Config) Setup() {
-	err := os.MkdirAll(config.Location, 755)
+func (osFS) Open(path string) (file, error)               { return os.Open(path) }
+func (osFS) MkdirAll(path string, perm fs.FileMode) error { return os.MkdirAll(path, perm) }
+func (osFS) ReadAll(r io.Reader) ([]byte, error)          { return ioutil.ReadAll(r) }
+
+func (config *Config) Setup() error {
+	err := fsym.MkdirAll(config.Location, 755)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "there was an issue creating the podcast file directory: %v", err)
+		return err
 	}
 
 	for _, podcast := range config.Podcasts {
 		// Create directories
 		fullPath := fmt.Sprintf("%v/%v", config.Location, podcast.Name)
-		err = os.MkdirAll(fullPath, 755)
+		err = fsym.MkdirAll(fullPath, 755)
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "error creating %s directory: %v", podcast.Name, err)
+			fmt.Fprintf(os.Stderr, "error creating %s directory: %v\n", podcast.Name, err)
 			os.Exit(1)
 		}
 	}
+	return nil
 }
 
-func Parse(path string) (*Config, []error) {
-
-	errors := []error{}
-	file, err := os.Open(path)
+func Parse(path string) (*Config, error) {
+	file, err := fsym.Open(path)
 	if err != nil {
-		errors = append(errors, fmt.Errorf("Unable to open configuration file: %v", err))
-		return nil, errors
+		return nil, err
 	}
 	defer file.Close()
 
-	bytes, err := ioutil.ReadAll(file)
+	bytes, err := fsym.ReadAll(file)
 	if err != nil {
-		errors = append(errors, fmt.Errorf("Unable to read configuration file: %v", err))
-		return nil, errors
+		return nil, err
 	}
 
 	var config Config
-	err = toml.Unmarshal(bytes, &config)
-	if err != nil {
-		errors = append(errors, fmt.Errorf("Unable to parse configuration file: %v", err))
-		return nil, errors
-	}
-
-	errors = append(errors, validate(&config)...)
-	if len(errors) > 0 {
-		return nil, errors
+	if err = toml.Unmarshal(bytes, &config); err != nil {
+		return nil, err
 	}
 	return &config, nil
-}
-
-func validate(config *Config) []error {
-	errors := []error{}
-	if config.Location == "" {
-		errors = append(errors, fmt.Errorf("\tConfig location is empty"))
-	}
-
-	for i, pod := range config.Podcasts {
-		if pod.Name == "" {
-			errors = append(errors, fmt.Errorf("\tPodcast #%d: config name is empty", i))
-		}
-		if pod.URL == "" {
-			errors = append(errors, fmt.Errorf("\tPodcast #%d: URL is empty", i))
-		}
-	}
-	return errors
 }
--- config/config_test.go
+++ config/config_test.go
@@ -1,69 +1,120 @@
 package config
 
 import (
+	"fmt"
+	"io"
+	"io/fs"
 	"testing"
 )
 
-type ValidationTests struct {
-	in   *Config
-	want int
+var errOpen = fmt.Errorf("Open is broken!")
+var errMkdirAll = fmt.Errorf("MkdirAll is broken!")
+var errReadAll = fmt.Errorf("ReadAll is broken!")
+
+type fakeFS struct {
+	openError     bool
+	mkdirAllError bool
+	readAllError  bool
+	contents      []byte
 }
 
-var ValidationConfigs = []*ValidationTests{
-	{
-		in: &Config{
-			Location: "",
-			Podcasts: nil,
-		},
-		want: 1,
-	},
-	{
-		in: &Config{
-			Location: "",
-			Podcasts: []*Podcast{
-				{
-					URL:  "www.google.com",
-					Name: "",
-				},
-				{
-					URL:  "",
-					Name: "Bill Bryson Sports Podcast",
-				},
-				{
-					URL:  "",
-					Name: "",
-				},
-			},
-		},
-		want: 5,
-	},
-	{
-		in: &Config{
-			Location: "A real location",
-			Podcasts: []*Podcast{
-				{
-					URL:  "www.valid.com",
-					Name: "Valid",
-				},
-				{
-					URL:  "www.veryvalid.com",
-					Name: "Very Valid",
-				},
-				{
-					URL:  "www.veryveryvalid.com",
-					Name: "Very Very Valid",
-				},
-			},
-		},
-		want: 0,
-	},
+type fakeFile struct {
+	file
 }
 
-func TestValidate(t *testing.T) {
-	for _, conf := range ValidationConfigs {
-		err := validate(conf.in)
-		if len(err) != conf.want {
-			t.Errorf("Test validate(config *Config), expected: %d errors, got: %d errors", conf.want, len(err))
+func (f fakeFile) Close() error {
+	return nil
+}
+
+func (f fakeFS) Open(path string) (file, error) {
+	if f.openError {
+		return nil, errOpen
+	}
+	return fakeFile{}, nil
+}
+func (f fakeFS) MkdirAll(path string, perm fs.FileMode) error {
+	if f.mkdirAllError {
+		return errMkdirAll
+	}
+	return nil
+}
+
+func (f fakeFS) ReadAll(r io.Reader) ([]byte, error) {
+	if f.readAllError {
+		return nil, errReadAll
+	}
+	return f.contents, nil
+}
+
+func TestParse(t *testing.T) {
+
+	var valid_toml = `location="/tmp"
+
+[[podcasts]]
+name="Philosophize This!"
+url="https://philosophizethis.libsyn.com/rss"
+
+[[podcasts]]
+name="Lex Fridman Podcast"
+url="https://lexfridman.com/feed/podcast"
+`
+
+	var tests = []struct {
+		file         string
+		openError    bool
+		readAllError bool
+		want         error
+	}{
+		{valid_toml, true, false, errOpen},
+		{valid_toml, false, true, errReadAll},
+		{valid_toml, true, true, errOpen},
+	}
+
+	for _, test := range tests {
+		// Create mock OS for testing
+		fsym = fakeFS{
+			test.openError,
+			false,
+			test.readAllError,
+			[]byte(test.file),
+		}
+		if _, got := Parse("fake path"); got != test.want {
+			t.Errorf("Parse() = %v, wanted: %v", got, test.want)
+		}
+	}
+}
+
+func TestSetup(t *testing.T) {
+	fsym = fakeFS{
+		false,
+		false,
+		false,
+		[]byte(``),
+	}
+
+	config, err := Parse("fake path")
+	if err != nil {
+		t.Errorf("Error constructing TestSetup initialization")
+	}
+
+	var tests = []struct {
+		mkdirError bool
+		want       error
+	}{
+		{false, nil},
+		{true, errMkdirAll},
+	}
+
+	for _, test := range tests {
+		// Create mock OS for testing
+		fsym = fakeFS{
+			false,
+			test.mkdirError,
+			false,
+			[]byte(nil),
+		}
+		if got := config.Setup(); got != test.want {
+			t.Errorf("Parse() = %v, wanted: %v", got, test.want)
 		}
 	}
 }
